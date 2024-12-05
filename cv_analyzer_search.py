249b63d885984255f020347f28a49b2fdc857675
bsiddhharth <bsiddhharth@gmail.com>
2024-12-05 22:54:26 +0530

    Updated the groq_api to deploy in streamlit , commanded the logger & decommanded the google_search_term, hours_old

--- cv_analyzer_search.py
+++ cv_analyzer_search.py
@@ -359,10 +359,10 @@ def Job_assistant():
                     jobs = scrape_jobs(
                         site_name=site_name,
                         search_term=search_term,
-                        # google_search_term=f"{search_term} jobs near {location}",
+                        google_search_term=f"{search_term} jobs near {location}",
                         location=location,
                         results_wanted=results_wanted,
-                        # hours_old=hours_old,
+                        hours_old=hours_old,
                         country_indeed=country_indeed,
                     )
                     st.session_state.job_search_results = jobs
